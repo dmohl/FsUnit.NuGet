c21ee2e559461f2580a469d144df7c9ab8c2e7c0
Dan Mohl <dmohl@yahoo.com>
2011-02-12 14:08:16 -0600

    .

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 c:\nuget\nuget.exe pack c:\git\FsUnit.NuGet\FsUnit.nuspec -b c:\git\FsUnit.NuGet -o c:\git\FsUnit.NuGet
-copy c:\git\FsUnit.NuGet\*.* c:\nuget\FsUnit.NuGet /Y
+md c:\nuget\FsUnit.NuGet\
+copy c:\git\FsUnit.NuGet\*.nupkg c:\nuget\FsUnit.NuGet\ /Y
 pause
